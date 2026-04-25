8c18c395190a55ebd6a8686c5d32449433a05a79
gopal-437 <gopalvaghela931@gmail.com>
2026-04-26 00:42:32 +0530

    Update agent api url to render deployment

--- cineflix-app/front-end/src/components/AgentChatWidget.js
+++ cineflix-app/front-end/src/components/AgentChatWidget.js
@@ -33,7 +33,7 @@ const AgentChatWidget = () => {
     setIsTyping(true);
 
     try {
-      const response = await fetch('http://localhost:8000/api/chat', {
+      const response = await fetch('https://cineflix-agent.onrender.com/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
